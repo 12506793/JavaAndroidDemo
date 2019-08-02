e9630dc02d46d7397294370d9c721c9ece4a380f
zty <zty>
2019-08-02 10:18:59 +0800

    init

---
+++ app/src/main/java/com/example/myapplication/view/Main2Activity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+/**
+ * @author wy
+ * SwipeRefreshLayout demo
+ */
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
